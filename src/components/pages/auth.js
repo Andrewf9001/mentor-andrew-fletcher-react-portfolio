55c27846892757da380b7e0d0ab3546487f300fa
AndrewMPFQ <andrewf9001@gmail.com>
2020-06-27 20:49:44 -0600

    Styled Auth component and created connection to the api

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
 
+import loginImg from "../../../static/assets/images/auth/login.jpg";
+import Login from "../auth/login";
+
 class auth extends Component {
   render() {
-    return <div>Auth...</div>;
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`,
+          }}
+        />
+
+        <div className="right-column">
+          <Login />
+        </div>
+      </div>
+    );
   }
 }
 
